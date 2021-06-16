b9a69413fea23236ccc5dafbcdfce1ff6f6cb5e5
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-16 13:16:30 -0500

    added routes for plants and water_logs, moved plant form to partial file 'plant_form', added partial to new and edit views and began show view for plants

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  get 'water_logs/index'
-  get 'water_logs/new'
-  get 'water_logs/create'
-  get 'water_logs/show'
-  get 'water_logs/edit'
-  get 'water_logs/update'
-  get 'water_logs/destroy'
-  get 'plants/index'
-  get 'plants/new'
-  get 'plants/create'
-  get 'plants/show'
-  get 'plants/edit'
-  get 'plants/update'
-  get 'plants/destroy'
+
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'omniauth'}
+  resources :plants
+  resources :waterlogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
