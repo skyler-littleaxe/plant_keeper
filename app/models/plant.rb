f5ed82530d9105885e3056af4e5016af711f66e1
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-16 23:30:14 -0500

    added validations to plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,7 @@
 class Plant < ApplicationRecord
     has_many :water_logs
     has_many :users, :through => :water_logs
+    validates :name, presence: true 
+    validates :water_need, presence: true
+    validates :water_need, inclusion: { in: ["Once, weekly", "Every other day", "Three times a week", "Daily", "Multiple times a day" ]}
 end
