63ae8262d221c6ae98c9aeb825716823364181e0
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-17 12:30:39 -0500

    added additional validation to plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,4 +4,5 @@ class Plant < ApplicationRecord
     validates :name, presence: true 
     validates :water_need, presence: true
     validates :water_need, inclusion: { in: ["Once, weekly", "Every other day", "Three times a week", "Daily", "Multiple times a day" ]}
+    validates :water_need, inclusion: { in: ["Dark", "Low Light", "Medium Light", "Sunny", "Outside" ]}
 end
