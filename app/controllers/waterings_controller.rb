490a632356a4beae6adcdf7988108537d4a9993f
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-17 15:19:40 -0500

    changed naming for routing of waterlogs to waterings

--- app/controllers/waterings_controller.rb
+++ app/controllers/waterings_controller.rb
@@ -4,13 +4,13 @@ class WateringsController < ApplicationController
  
 
   def new
-    @water_log = WaterLog.new
+    @watering = Watering.new
   end
 
   def create
-    @water_log = WaterLog.new(waterlog_params)
-    if @plant.save
-      redirect_to plants_index_path, notice: "Water Log added to #{@water_logs.plant.name}."
+    @watering = Watering.new(watering_params)
+    if @watering.save
+      redirect_to plants_index_path, notice: "Watering Log added to #{@watering.plant.name}."
     else
       render :new
     end
@@ -23,16 +23,22 @@ class WateringsController < ApplicationController
   end
 
   def update
-    if @water_log = WaterLog.update(waterlog_params)
-      redirect_to plants_index_path, notice: "Water Log for #{@water_logs.plant.name} successfully updated."
+    if @watering = Watering.update(watering_params)
+      redirect_to plants_index_path, notice: "Water Log for #{@watering.plant.name} successfully updated."
     else
       render :edit
     end
   end
 
   def destroy
-    plant_name = @water_log.plant.name
-    @water_log.destroy
+    plant_name = @watering.plant.name
+    @watering.destroy
     redirect_to plants_index_path, notice: "Water Log for #{plant_name} successfully deleted."
+  end
+
+  private
+
+  def watering_params
+    params.require(:watering).permit(:water_date, :water_time, :user_id, :plant_id )
   end
 end
