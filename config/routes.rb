1d86938e5aa3aaad7ad7b66a4dec6016071ea826
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-17 20:47:24 -0500

    added root logic for authenticated and nonauthenticated users in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'omniauth'}
+  authenticated :user do
+    root 'home#welcome', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'user/sessions#new'
+  end
   resources :plants
   resources :waterings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
