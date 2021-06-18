2c5103c327b219e69ca4003d0112eef4fc0af9a0
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2021-06-17 21:29:47 -0500

    created new oauth for facebook - including omniauth_callbacks_controller file with callback logic, appropriate macro, update to credentials and initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
          def self.create_from_provider_data(provider_data)
           where(provider: provider_data.provider, uid: provider_data.uid).first_or_create  do |user|
